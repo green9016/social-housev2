1a302eb950bd5fca9083982d3b1f401c2522d326
green9016 <marss9016@gmail.com>
2022-10-12 02:20:28 +0800

    fix: style && paper

--- src/utils/paper.mjs
+++ src/utils/paper.mjs
@@ -1,5 +1,5 @@
 // export const sdkClientSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdElkIjoiNTYzYjYxMWMtMGFmYy00MDNmLWE0YmItMDY5NTlkNDgzMjU0IiwicXVhbnRpdHkiOjEsInVzZVBhcGVyS2V5IjpmYWxzZSwibWV0YWRhdGEiOnt9LCJleHBpcmVzSW5NaW51dGVzIjoxNSwiaGlkZUFwcGxlUGF5R29vZ2xlUGF5IjpmYWxzZSwidGl0bGUiOiIiLCJjYXB0dXJlUGF5bWVudExhdGVyIjpmYWxzZSwibWludE1ldGhvZCI6eyJuYW1lIjoibWludCIsImFyZ3MiOnt9LCJjYWxsT3B0aW9ucyI6eyJnYXNQcmlvcml0eSI6Im1lZGl1bSJ9LCJwYXltZW50Ijp7ImN1cnJlbmN5IjoiRVRIIiwidmFsdWUiOiIwLjAwMiJ9fSwicHJpY2luZ0RldGFpbHMiOnsiY2hhaW5OYW1lIjoiR29lcmxpIiwiY3VycmVuY3lBZGRyZXNzIjoiMHgwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwiY3VycmVuY3lTeW1ib2wiOiJFVEgiLCJyZWFkYWJsZVByaWNlIjoiMC4wMDIiLCJwcmljZUluV2VpIjp7InR5cGUiOiJCaWdOdW1iZXIiLCJoZXgiOiIweDA3MWFmZDQ5OGQwMDAwIn0sImxvY2tlZFByaWNlVXNkQ2VudHMiOjI3M30sInRyYW5zYWN0aW9uSWQiOiI1NjVjNzcyNS1kZGRjLTQ1NGYtODQ2YS05MjdkODVhZDZiNTIiLCJpYXQiOjE2NjUxMTEyMDQsImV4cCI6MTY2NTExMjEwNCwiaXNzIjoicGFwZXIueHl6In0.qg5Pc9KZ2BV0v6ywYl3CWfoFS70EdlrZ2eVjFcyWYg4'
-const CONTRACT_ID = '563b611c-0afc-403f-a4bb-06959d483254'
+const CONTRACT_ID = 'dcf4d482-93f7-4f1c-8112-58e7c6065d1b'
 const PAPER_API_KEY = process.env.PAPER_API_KEY || 'd36b2479-3b90-4987-b94e-84b49fe094a2'
 const EXPIRES_IN_MINS = 15
 
@@ -26,7 +26,9 @@ export const getPaperSdkClientSecret = async ({
         expiresInMinutes: EXPIRES_IN_MINS,
         mintMethod: {
           name: "mint",
-          args: {},
+          args: {
+            "_address": "$WALLET"
+          },
           payment: {
             value: priceInEth,
             currency: "ETH"
